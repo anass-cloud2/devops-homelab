da7f234e3415101f3c82bfc09085936466801bd7
anass-cloud2 <eenacirianass9@gmail.com>
2026-06-14 17:32:06 +0100

    Initial commit: Flask app + automated setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "=== DevOps Home Lab Setup ==="
+
+echo "[1/4] Updating system..."
+sudo apt update -y
+
+echo "[2/4] Installing Python3 and pip..."
+sudo apt install python3 python3-pip -y
+
+echo "[3/4] Installing Flask..."
+pip3 install flask --break-system-packages
+
+echo "[4/4] Starting the app..."
+python3 app/app.py
